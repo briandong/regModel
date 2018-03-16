e56560236056c79b46e4f93c852b84e975e94061
1nf1n1ty <bo.dong@outlook.com>
2018-03-16 09:34:03 +0800

    Update example_dut.sv

--- example/example_dut.sv
+++ example/example_dut.sv
@@ -8,7 +8,7 @@ assign apb.prdata = (apb.psel && apb.penable && !apb.pwrite) ? pr_data : 'z;
 
 reg [31:0] REG_ID;
 reg [31:0] REG_DATA;
-//reg [63:0] SOCKET[256];
+reg [31:0] REG_CLUSTER[8];
 reg [31:0] MEM_RAM[1024];
 
 always @ (posedge apb.pclk)
@@ -16,9 +16,9 @@ always @ (posedge apb.pclk)
    if (rst) begin
       REG_ID <= {4'h0, 10'h176, 8'h5A, 8'h03};
       REG_DATA <= 'h00;
-      //foreach (SOCKET[i]) begin
-      //   SOCKET[i] <= 64'h0000_0000;
-      //end
+      foreach (REG_CLUSTER[i]) begin
+         REG_CLUSTER[i] <= 32'h0;
+      end
       pr_data <= 32'h0;
    end
    else begin
@@ -29,18 +29,16 @@ always @ (posedge apb.pclk)
          if (apb.pwrite) begin
             casex (apb.paddr)
               16'h0104: REG_DATA <= apb.pwdata;
-              //16'h1XX0: SOCKET[apb.paddr[11:4]][63:32] <= apb.pwdata; 
-              //16'h1XX4: SOCKET[apb.paddr[11:4]][31: 0] <= apb.pwdata;
-              16'h011X: MEM_RAM[apb.paddr[11:2]] <= apb.pwdata;
+              16'h011X, 16'h012X: REG_CLUSTER[(apb.paddr-'h110)>>2] <= apb.pwdata; 
+              16'h02XX: MEM_RAM[(apb.paddr-'h200)>>2] <= apb.pwdata;
             endcase
          end
          else begin
             casex (apb.paddr)
               16'h0100: pr_data <= REG_ID;
               16'h0104: pr_data <= REG_DATA;
-              //16'h1XX0: pr_data <= SOCKET[apb.paddr[11:4]][63:32];
-              //16'h1XX4: pr_data <= SOCKET[apb.paddr[11:4]][31: 0];
-              16'h011X: pr_data <= MEM_RAM[apb.paddr[11:2]];
+              16'h011X, 16'h012X: pr_data <= REG_CLUSTER[(apb.paddr-'h110)>>2];
+              16'h02XX: pr_data <= MEM_RAM[(apb.paddr-'h200)>>2];
             endcase
          end
       end
